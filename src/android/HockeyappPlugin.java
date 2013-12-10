8c58513bf741da93926698d3216e11bf6a8b5ead
Stefan Mayer <s.mayer@netconomy.net>
2013-12-10 11:43:45 +0100

    SPOSP-357 Fixed issue in Android src

--- src/android/HockeyappPlugin.java
+++ src/android/HockeyappPlugin.java
@@ -8,10 +8,12 @@ import net.hockeyapp.android.UpdateManager;
 
 public class HockeyappPlugin extends CordovaPlugin {
     private static String APP_ID = "1daef6208b0180c53283c1b28a167e24";
+    private static CordovaInterface _cordova;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
+        this._cordova = cordova;
         checkForUpdates();
     }
 
@@ -22,12 +24,12 @@ public class HockeyappPlugin extends CordovaPlugin {
     }
 
     private void checkForCrashes() {
-       CrashManager.register(this, APP_ID);
+       CrashManager.register(this._cordova.getActivity(), APP_ID);
      }
 
      private void checkForUpdates() {
        // Remove this for store builds!
-       UpdateManager.register(this, APP_ID);
+       UpdateManager.register(this._cordova.getActivity(), APP_ID);
      }
 
 }
